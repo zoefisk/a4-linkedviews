cad90546e693820352357b1b5d2fbda2538e7d67
zoefisk <94054962+zoefisk@users.noreply.github.com>
2026-02-28 12:59:45 -0500

    update next.config to work for github pages

--- a4/next.config.ts
+++ a4/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const repoName = "a4-linkedviews";
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: "export", // ✅ makes /out for GitHub Pages
+    basePath: isProd ? `/${repoName}` : "",
+    assetPrefix: isProd ? `/${repoName}/` : "",
+    images: {
+        unoptimized: true, // ✅ required for static export on Pages
+    },
 };
 
 export default nextConfig;
